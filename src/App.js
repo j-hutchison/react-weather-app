124f068708f4a09617efc24a15e2c6e94039ccd0
j-hutchison <jhutchison@outlook.de>
2022-08-09 15:37:53 +0200

    Completed solution incl. API using openweather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import keys from "./keys.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [searchLocation, setSearchLocation] = useState("");
+	const [location, setLocation] = useState("-");
+	const [date, setDate] = useState(new Date(Date.now()).toUTCString());
+	const [temperature, setTemperature] = useState("-");
+	const [forecast, setForecast] = useState("-");
+
+	const API_KEY = keys.API_KEY;
+	const API_URL = keys.WEATHER_URL;
+
+	const handleSearchLocationChange = (e) => {
+		setSearchLocation(() => e.target.value);
+	};
+
+	async function getLocationData() {
+		console.log(`${API_URL}q=${searchLocation}&appid=${API_KEY}`);
+
+		const response = await fetch(
+			`${API_URL}q=${searchLocation}&appid=${API_KEY}`
+		);
+
+		return response.json();
+	}
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log(searchLocation);
+		const data = getLocationData().then((json) => {
+			console.log(json);
+			const {
+				name: city,
+				main: { temp },
+				weather,
+			} = json;
+
+			setLocation(() => city);
+			setTemperature(() => temp);
+			setForecast(() => weather[0].main);
+		});
+
+		setSearchLocation(() => "");
+	};
+
+	return (
+		<div className="app">
+			<form
+				className={temperature < 15 ? "cold" : "warm"}
+				onSubmit={handleFormSubmit}
+			>
+				<div className="search-box">
+					<input
+						className="search-box-input"
+						type="text"
+						value={searchLocation}
+						onChange={handleSearchLocationChange}
+						placeholder="Search..."
+					/>
+				</div>
+				<div className="weather-details">
+					<h1 className="weather-details-location">{location}</h1>
+					<em className="weather-details-date">{date}</em>
+					<strong className="weather-details-temp">{temperature}&#xb0;C</strong>
+					<strong className="weather-details-forecast">{forecast}</strong>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
