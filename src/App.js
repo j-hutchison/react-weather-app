c384c777e7e992d221c1444c8565888cba642faf
j-hutchison <jhutchison@outlook.de>
2022-08-09 16:00:42 +0200

    CSS Style changes to date

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
-import keys from "./keys.json";
+import api from "./api.json";
 
 function App() {
 	const [searchLocation, setSearchLocation] = useState("");
 	const [location, setLocation] = useState("-");
-	const [date, setDate] = useState(new Date(Date.now()).toUTCString());
 	const [temperature, setTemperature] = useState("-");
 	const [forecast, setForecast] = useState("-");
 
-	const API_KEY = keys.API_KEY;
-	const API_URL = keys.WEATHER_URL;
+	const API_KEY = api.key;
+	const API_URL = api.base;
 
 	const handleSearchLocationChange = (e) => {
 		setSearchLocation(() => e.target.value);
@@ -62,7 +61,9 @@ function App() {
 				</div>
 				<div className="weather-details">
 					<h1 className="weather-details-location">{location}</h1>
-					<em className="weather-details-date">{date}</em>
+					<em className="weather-details-date">
+						{new Date(Date.now()).toUTCString()}
+					</em>
 					<strong className="weather-details-temp">{temperature}&#xb0;C</strong>
 					<strong className="weather-details-forecast">{forecast}</strong>
 				</div>
